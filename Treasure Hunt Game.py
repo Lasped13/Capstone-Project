b71e4d921a682b684f78cd1dd29747ae6e8f9f96
Lasped <pedropenelas@hotmail.co.uk>
2024-03-28 18:44:40 +0000

    Started Treasure Hunt Game

---
+++ Treasure Hunt Game.py
@@ -0,0 +1,6 @@
+"""
+This is a program that allows the user to play a mini Treasure Hunt Game
+This will be done on a grid where the user will enter coordinates to move around the grid.
+The mechanics are similar to Minesweeper in the sense that some grid spaces will give
+them points whereas others will reduce their points
+"""
